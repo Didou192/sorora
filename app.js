616625bdbb7f1c0794d0841d99c6da3748d0b782
Didou192 <ortizanna082@gmail.com>
2025-07-30 12:24:18 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,4 @@
+alert("YO, HAHAHAHA, VOUS VENEZ DE VOUS FAIRE HACKER UNE SECONDE FOIS, BONNE CHANCE POUR NOUS RETROUVER ! ");
+setTimeout(function() {
+    window.location.href = "https://sana01222.github.io/error-404-girls/";
+}, 1000);
